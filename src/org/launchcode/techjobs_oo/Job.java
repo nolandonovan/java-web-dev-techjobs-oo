fb872785c871862133ad8647f55903f3d2780af3
nolandonovan <40967397+nolandonovan@users.noreply.github.com>
2020-02-05 20:46:02 -0600

    testJobToStringFormat complete

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -84,13 +84,15 @@ public class Job {
         return Objects.hash(getId());
     }
 
-    // TODO: Add two constructors - one to initialize a unique ID and a second to initialize the
-    //  other five fields. The second constructor should also call the first in order to initialize
-    //  the 'id' field.
-
-    // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
-    //  match.
-
-    // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
-    //  and id.
+    @Override
+    public String toString() {
+        return "\n" +
+                "ID: " + id + "\n" +
+                "Name: " + name + "\n" +
+                "Employer: " + employer + "\n" +
+                "Location: " + location + "\n" +
+                "Position Type: " + positionType + "\n" +
+                "Core Competency: " + coreCompetency +
+                "\n" ;
+    }
 }
--- src/org/launchcode/techjobs_oo/tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/tests/JobTest.java
@@ -3,16 +3,34 @@ package org.launchcode.techjobs_oo.tests;
 import org.junit.Before;
 import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
+import java.util.regex.Pattern;
 import static org.junit.Assert.*;
+
 
 public class JobTest {
     Job jobOne;
     Job jobTwo;
+    Job jobThree;
+    Job jobFour;
 
     @Before
     public void createJobObjects(){
-        Job jobOne = new Job();
-        Job jobTwo = new Job();
+        jobOne = new Job();
+        jobTwo = new Job();
+        jobThree = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
+        jobFour = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence")
+        );
     }
 
     @Test
@@ -21,4 +39,36 @@ public class JobTest {
         assertTrue((jobTwo.getId() - jobOne.getId()) == 1);
     }
 
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        // test Job constructor sets the correct class and value for each field
+        // fields: id, name, employer, location, positionType, and coreCompetency
+        // data types: int, String, Employer, Location, PositionType, and CoreCompetency
+
+        //test classes
+        assertTrue( jobThree instanceof  Job );
+        assertTrue( jobThree.getId() == (int) jobThree.getId());
+        assertTrue( jobThree.getName() instanceof  String );
+        assertTrue( jobThree.getEmployer() instanceof  Employer );
+        assertTrue( jobThree.getLocation() instanceof  Location );
+        assertTrue( jobThree.getPositionType() instanceof  PositionType );
+        assertTrue( jobThree.getCoreCompetency() instanceof  CoreCompetency );
+
+        //test values
+        assertEquals("Product tester", jobThree.getName());
+        assertEquals("ACME", jobThree.getEmployer().getValue());
+        assertEquals("Desert", jobThree.getLocation().getValue());
+        assertEquals("Quality control", jobThree.getPositionType().getValue());
+        assertEquals("Persistence", jobThree.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        assertFalse(jobThree.equals(jobFour));
+    }
+
+    @Test
+    public void testJobToStringFormat(){
+        assertTrue(Pattern.matches("\n(.*\n){6}" , jobThree.toString() ) );
+    }
 }
