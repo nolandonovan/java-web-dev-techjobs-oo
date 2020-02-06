aef3a2ead979840a3cf5a9e823fc2ad8de9e0b57
nolandonovan <40967397+nolandonovan@users.noreply.github.com>
2020-02-05 19:13:31 -0600

    created JobTest tests

--- src/org/launchcode/techjobs_oo/tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/tests/JobTest.java
@@ -1,5 +1,24 @@
 package org.launchcode.techjobs_oo.tests;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+import static org.junit.Assert.*;
+
 public class JobTest {
+    Job jobOne;
+    Job jobTwo;
+
+    @Before
+    public void createJobObjects(){
+        Job jobOne = new Job();
+        Job jobTwo = new Job();
+    }
+
+    @Test
+    public void testSettingJobId(){
+        assertFalse(jobOne.getId() == jobTwo.getId());
+        assertTrue((jobTwo.getId() - jobOne.getId()) == 1);
+    }
 
 }
